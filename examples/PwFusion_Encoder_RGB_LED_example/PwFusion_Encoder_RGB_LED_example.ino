5a40b37e5f720cb170ca38475e0bb6e786bea180
Noah J <111708042+BasementMaker1@users.noreply.github.com>
2024-01-11 10:03:12 -0600

    Update license

--- examples/PwFusion_Encoder_RGB_LED_example/PwFusion_Encoder_RGB_LED_example.ino
+++ examples/PwFusion_Encoder_RGB_LED_example/PwFusion_Encoder_RGB_LED_example.ino
@@ -6,26 +6,27 @@
 * Designed for use with with Playing With Fusion I2C Rotary Encoder
 * interface board: IFB-40001
 *
-* Copyright © 2015-18 Playing With Fusion, Inc.
-* SOFTWARE LICENSE AGREEMENT: This code is released under the MIT License.
+* MIT License
 *
-* Permission is hereby granted, free of charge, to any person obtaining a
-* copy of this software and associated documentation files (the "Software"),
-* to deal in the Software without restriction, including without limitation
-* the rights to use, copy, modify, merge, publish, distribute, sublicense,
-* and/or sell copies of the Software, and to permit persons to whom the
-* Software is furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
+* Copyright (c) 2024 Playing With Fusion, Inc
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-* DEALINGS IN THE SOFTWARE.
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
 * **************************************************************************
 * REVISION HISTORY:
 * Author		  Date	    Comments
@@ -37,7 +38,7 @@
 * **************************************************************************
 * ADDITIONAL NOTES:
 * This file contains functions to initialize and run a Playing With Fusion R3aktor in
-* order to communicate with the I2C Encoder board. Funcionality is as described below:
+* order to communicate with the I2C Encoder board. Functionality is as described below:
 *	- Configure R3aktor to alter the brightness of an RGB LED based on the input from
 *   the rotary encoder
 *       - call PWF library to configure and read the I2C Rotary Encoder inputs
